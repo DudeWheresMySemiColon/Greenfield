33af0d2bc41f836233637ae77c57ae9bdf1d39c8
Jake Garelick <jakegarelick@gmail.com>
2015-10-22 15:04:50 -0700

    feat: server side auth

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,13 +1,15 @@
 var morgan      = require('morgan'), // used for logging incoming request
     bodyParser  = require('body-parser')
 
-   module.exports = function(app,express){
+   module.exports = function(app, express){
   	app.use(morgan('dev'));
   	app.use(bodyParser.urlencoded({extended: true}));
   	app.use(bodyParser.json());
   	app.use(express.static(__dirname + '/../../client'));
+
+    //what is this?
   	app.use('api/users');
   	app.use('api/users');
   	app.use('app/meals');
   	app.use('api/orders');
-   }
+   };
---
+++ server/users/usersRoutes.js
@@ -0,0 +1,11 @@
+var userController = require('./userController.js');
+
+
+module.exports = function (app) {
+
+  app.post('/customer/post/signin', userController.signin);
+  app.post('/customer/post/signup', userController.signup);
+
+  app.get('/customer/get/signedin', userController.checkAuth);
+
+};
